5ea1bb73a0dd0c72e2ea3ca2dc529f7ec4f170bc
6im0n <simon.gl3135@orange.fr>
2026-04-30 11:52:42 +0200

    :sparkles: Add lab7_1.py for optical flow analysis and update requirements.txt with opencv and matplotlib

---
+++ lab_7/lab7_1.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+# the lab learn to become familliar with optical flow :
+# - optical flow is a vector field describing the movement of pixels betwen two consicuted frame (in video sequence for exemple)
+
+
